bd5101a4db8ca06dcfc8fb672f257789286d277d
moubtahijbilalz <bilalmoubtahij123@gmail.com>
2023-03-16 14:29:45 -0700

    a

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * DESCRIPTION:A C program that prints with pritf */
+int main(void){
+
+
+    printf("with proper grammar, but the outcome is a piece of art");
+
+return (0);
+}
