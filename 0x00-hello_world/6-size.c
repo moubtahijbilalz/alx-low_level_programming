0955586d3222770efa999de05fd195c15ace399d
moubtahijbilalz <bilalmoubtahij123@gmail.com>
2023-03-16 15:22:34 -0700

    a

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: using the sizeof to prints size of various type.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+	printf("size of a chat: %lu byte(s)∖n",sizeof(char));
+	printf("size of an int: %lu byte(s)∖n",sizeof(int));
+	printf("size of a long int: %lu byte(s)∖n",sizeof(long int));
+	printf("size of a long long int: %lu byte(s)∖n",sizeof(long long int));
+	printf("size of a float: %lu byte(s)∖n",sizeof(float));
+	return (0);
+}
