697698e3295b5a092c92716ef1e7262b6fc799f6
moubtahijbilalz <bilalmoubtahij123@gmail.com>
2023-03-21 11:34:41 -0700

    b

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,13 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
 
-	/**
-	 * main - check the code
-	 *
-	 * Return: Always 0.
-	 */
-	int main(void)
-	{
-	    times_table();
-	    return (0);
-	}
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
+}
