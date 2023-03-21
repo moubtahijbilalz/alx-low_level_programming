be5c39934dcb9983d8a7d9d8ab7aed957769ffb4
moubtahijbilalz <bilalmoubtahij123@gmail.com>
2023-03-21 08:27:18 -0700

    a

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,13 +1,37 @@
 #include "main.h"
+		
+/**
+		
+ * main - check the code.
+		
+ *
+		
+ * Return: Always 0.
+		
+ */
+		
+int main(void)
+		
+{
+		
+    int r;
+		
 
-
-	/**
-	 * main - check the code
-	 *
-	 * Return: Always 0.
-	 */
-	int main(void)
-	{
-	    print_alphabet();
-	    return (0);
-	}
+		
+    r = _islower('H');
+		
+    _putchar(r + '0');
+		
+    r = _islower('o');
+		
+    _putchar(r + '0');
+		
+    r = _islower(108);
+		
+    _putchar(r + '0');
+		
+    _putchar('\n');
+		
+    return (0);
+		
+}
