c4774a527a1cdb85e38dc208921b8514dc8264f4
moubtahijbilalz <bilalmoubtahij123@gmail.com>
2023-03-21 10:12:41 -0700

    b

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,18 +1,12 @@
 #include "main.h"
-	
+
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(void)		
-{	
-    int r;
-
-    print_last_digit(98);
-    print_last_digit(0);
-    r = print_last_digit(-1024);
-    _putchar('0' + r);
-    _putchar('\n');
+int main(void)
+{
+    jack_bauer();
     return (0);
 }
