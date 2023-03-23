bd85a1b985737ee4e9841cc019d24ba5dea6b19d
moubtahijbilalz <bilalmoubtahij123@gmail.com>
2023-03-23 12:46:30 -0700

    1-main file

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,15 +9,14 @@ int main(void)
 {
 	int i;
 
-	printf("Infiniteprint loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 
-	while (I < 10)
-	{
-		putchar(i + '0');
-		i++;
-	}
+	/*while (i < 10) */
+	/*{ */
+	/*      putchar(i);*/
+	/*} */
 
 	printf("Infinite loop avoided! \\o/\n");
 
